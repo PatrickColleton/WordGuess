bb769c051772b8a2e79efa71f56afe5bc2b5a3f6
PatrickColleton <patrickcolleton@gmail.com>
2017-10-27 15:12:33 -0400

    functional hangman with no css

--- js/index.js
+++ js/index.js
@@ -1,64 +1,97 @@
 const form = document.forms[0];
 const numTries = document.getElementById('tries');
 const resultDiv = document.getElementById('result');
+const partialDiv = document.getElementById('partial');
 const restartBtn = document.getElementById('restart');
+const submitBtn = document.getElementById('submit');
 const prevTriesSpan = document.getElementById('prev-tries');
+var answerWord = '';
+var partialAnswer = '';
+var api = 'http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=false&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5';
+
+partialDiv.textContent = partialAnswer;
+
 // Game Data
-let triesCounter, answer, prevTries, timeoutId;
+let triesCounter, prevTries, timeoutId;
 // Game Logic
-function startGame(){
+function startGame() {
   triesCounter = 0;
   numTries.textContent = triesCounter;
   prevTries = [];
   resultDiv.textContent = '';
   prevTriesSpan.textContent = '';
-  answer = Math.floor((Math.random()*100)+1);
+
+  fetch(api).then(resp => resp.json()).then(function(data) {
+    answerWord = data.word;
+    console.log(answerWord);
+    for (let i = 0; i < answerWord.length; i++) {
+      partialAnswer += '_';
+    }
+    console.log(partialAnswer);
+  });
 }
-function clearResultDiv(){
-  timeoutId = setTimeout(function(){
+
+function clearResultDiv() {
+  timeoutId = setTimeout(function() {
     resultDiv.textContent = '';
   }, 3000);
 }
-function renderPrevTries(){
-  prevTriesSpan.textContent = prevTries.sort((a,b)=> a-b).join(', ');
+
+function renderPrevTries() {
+  prevTriesSpan.textContent = prevTries.sort((a, b) => a - b).join(', ');
 }
-function isValidGuess(guess){
-  return guess && guess >= 1 && guess <= 100 && !prevTries.includes(guess);
-}
-// Event Listeners
-form.addEventListener('submit', function(event){
+
+
+form.addEventListener('submit', function(event) {
   event.preventDefault();
 
-  const guess = parseInt(form.guess.value, 10);
-  console.log(guess);
+  const rawGuess = form.guess.value;
+  let guess = rawGuess.toLowerCase();
 
-  clearTimeout(timeoutId);
-
-  if(isValidGuess(guess)){
+  if (! prevTries.includes(guess) && guess != '') {
+    prevTries.push(guess);
+    clearTimeout(timeoutId);
     triesCounter++;
     numTries.textContent = triesCounter;
+  }
+  renderPrevTries();
 
-    prevTries.push(guess);
-    renderPrevTries();
+  if (answerWord.includes(guess) && guess !== '') {
+    resultDiv.textContent = 'Right Letter!';
 
-     if(guess === answer){
-       resultDiv.textContent = 'You win!';
-       restartBtn.classList.remove('hidden');
-     } else if (guess > answer){
-       resultDiv.textContent = 'Guess lower.';
-       clearResultDiv();
-     } else {
-       resultDiv.textContent = 'Guess higher.';
-       clearResultDiv();
-     }
-   } else {
-     resultDiv.textContent = 'Input must be an integer between 1 and 100 that you have not guessed.';
-   }
+    let partialArray = partialAnswer.split('');
+
+    for (let i = 0; i < answerWord.length; i++) {
+      if (guess === answerWord.charAt(i)) {
+        partialArray[i] = guess;
+      }
+    }
+    partialAnswer = partialArray.join('');
+
+    partialDiv.textContent = partialAnswer;
+
+    if (partialAnswer == answerWord) {
+      resultDiv.textContent = 'You win!';
+      restartBtn.classList.remove('hidden');
+      partialDiv.textContent = '';
+      partialAnswer = '';
+      clearResultDiv();
+    }
+  } else {
+    if (guess !== '') {
+      resultDiv.textContent = 'Wrong Letter!';
+    }
+    clearResultDiv();
+  }
 
   form.guess.value = '';
 });
-restartBtn.addEventListener('click', startGame);
-window.addEventListener('load', function(){
+
+restartBtn.addEventListener('click', function() {
+  startGame();
+  restartBtn.classList.add("hidden");
+})
+window.addEventListener('load', function() {
   startGame();
 })
 // Add Comment Collapse
